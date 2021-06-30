6d37eb3730aa5506acfcdeb764c70cb0bdf32584
RealMurciaGrupoApp <86695172+RealMurciaGrupoApp@users.noreply.github.com>
2021-06-30 13:00:43 +0200

    Create events.js

---
+++ events.js
@@ -0,0 +1,12 @@
+function events() {
+    return [
+        {
+        title: 'All Day Event',
+        start: '2021-06-01'
+        },
+        {
+        title: 'All Day Event',
+        start: '2021-08-25'
+        }
+    ];
+  }
