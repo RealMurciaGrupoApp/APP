07514a5b77cba0dea39e72e1bf91af7f00711973
RealMurciaGrupoApp <86695172+RealMurciaGrupoApp@users.noreply.github.com>
2021-07-24 21:04:01 +0200

    Update Events

--- events.js
+++ events.js
@@ -1,12 +1,140 @@
 function events() {
     return [
         {
-        title: 'All Day Event',
-        start: '2021-06-01'
+        title: 'Real Murcia CF - CD Marchamalo',
+        start: '2021-09-05'
         },
         {
-        title: 'All Day Event',
-        start: '2021-08-25'
+        title: 'Club Recreativo Granada - Real Murcia CF',
+        start: '2021-09-12'
+        },
+        {
+        title: 'Real Murcia CF - CF Intercity',
+        start: '2021-09-19'
+        },
+        {
+        title: 'Mar Menor CF - Real Murcia CF',
+        start: '2021-09-26'
+        },
+        {
+        title: 'Real Murcia CF - UD Alzira',
+        start: '2021-10-03'
+        },
+        {
+        title: 'UD Socuéllamos CF - Real Murcia CF ',
+        start: '2021-10-10'
+        },
+        {
+        title: 'Real Murcia CF - Club Atlético Pulpileño',
+        start: '2021-10-17'
+        },
+        {
+        title: 'Atlético Levante UD - Real Murcia CF ',
+        start: '2021-10-24'
+        },
+        {
+        title: 'Real Murcia CF - CD El Ejido 2012',
+        start: '2021-10-31'
+        },
+        {
+        title: 'CF La Nucía - Real Murcia CF ',
+        start: '2021-11-07'
+        },
+        {
+        title: 'Real Murcia CF - CD Eldense',
+        start: '2021-11-14'
+        },
+        {
+        title: 'Hércules de Alicante CF - Real Murcia CF ',
+        start: '2021-11-21'
+        },
+        {
+        title: 'Calvo Sotelo Puertollano CF - Real Murcia CF ',
+        start: '2021-11-28'
+        },
+        {
+        title: 'Real Murcia CF - UD Melilla',
+        start: '2021-12-05'
+        },
+        {
+        title: 'Águilas FC - Real Murcia CF ',
+        start: '2021-12-12'
+        },
+        {
+        title: 'Real Murcia CF - Atlético Mancha Real',
+        start: '2021-12-19'
+        },
+        {
+        title: 'CD Toledo - Real Murcia CF ',
+        start: '2022-01-09'
+        },
+        {
+        title: 'CD Marchamalo - Real Murcia CF ',
+        start: '2022-01-23'
+        },
+        {
+        title: 'Real Murcia CF - Club Recreativo Granada',
+        start: '2022-01-30'
+        },
+        {
+        title: 'CF Intercity - Real Murcia CF ',
+        start: '2022-02-06'
+        },
+        {
+        title: 'Real Murcia CF - Mar Menor CF',
+        start: '2022-02-13'
+        },
+        {
+        title: 'UD Alzira - Real Murcia CF ',
+        start: '2022-02-20'
+        },
+        {
+        title: 'Real Murcia CF - UD Socuéllamos CF',
+        start: '2022-02-27'
+        },
+        {
+        title: 'Club Atlético Pulpileño - Real Murcia CF ',
+        start: '2022-03-06'
+        },
+        {
+        title: 'Real Murcia CF - Atlético Levante UD',
+        start: '2022-03-13'
+        },
+        {
+        title: 'CD El Ejido 2012 - Real Murcia CF ',
+        start: '2022-03-20'
+        },
+        {
+        title: 'Real Murcia CF - CF La Nucía',
+        start: '2022-03-27'
+        },
+        {
+        title: 'CD Eldense - Real Murcia CF ',
+        start: '2022-04-03'
+        },
+        {
+        title: 'Real Murcia CF - Hércules de Alicante CF ',
+        start: '2022-04-10'
+        },
+        {
+        title: 'Real Murcia CF - Calvo Sotelo Puertollano CF',
+        start: '2022-04-17'
+        },
+        {
+        title: 'UD Melilla - Real Murcia CF ',
+        start: '2022-04-24'
+        },
+        {
+        title: 'Real Murcia CF - Águilas FC',
+        start: '2022-05-01'
+        },
+        {
+        title: 'Atlético Mancha Real - Real Murcia CF ',
+        start: '2022-05-08'
+        },
+        {
+        title: 'Real Murcia CF - CD Toledo ',
+        start: '2022-05-15'
         }
     ];
   }
